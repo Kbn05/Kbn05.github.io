dc0abe2f5e8868fe8e61026c60bd67288f5fd763
Kevin <kpaboncarvajal@gmail.com>
2025-07-18 23:42:28 -0500

    Add: Projects section & demos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  @ViewChild('eventAppModal') eventAppModalRef!: ElementRef<HTMLDivElement>;
+  @ViewChild('eventAppPlayer') eventAppVideoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('pomModal') pomModalRef!: ElementRef<HTMLButtonElement>;
+  @ViewChild('pomPlayer') pomVideoRef!: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    // Aquí ya puedes usar modalRef y videoRef si necesitas hacer algo apenas el DOM esté listo.
+  }
+
+  showEventAppVideo(): void {
+    const modal = this.eventAppModalRef.nativeElement;
+    const video = this.eventAppVideoRef.nativeElement;
+
+    modal.style.display = 'block';
+    video.currentTime = 0;
+    video.play();
+  }
+
+  closeEventAppVideo(): void {
+    const modal = this.eventAppModalRef.nativeElement;
+    const video = this.eventAppVideoRef.nativeElement;
+
+    modal.style.display = 'none';
+    video.pause();
+  }
+
+  showPomVideo(): void {
+    const modal = this.pomModalRef.nativeElement;
+    const video = this.pomVideoRef.nativeElement;
+
+    modal.style.display = 'block';
+    video.currentTime = 0;
+    video.play();
+  }
+  closePomVideo(): void {
+    const modal = this.pomModalRef.nativeElement;
+    const video = this.pomVideoRef.nativeElement;
+
+    modal.style.display = 'none';
+    video.pause();
+  }
 
 }
